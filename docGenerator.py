71220c0900db3a98dbbace85f53fe63d4c469bfa
oeway <oeway007@gmail.com>
2013-08-06 16:28:47 +0800

    new parse rule

--- docGenerator.py
+++ docGenerator.py
@@ -5,8 +5,14 @@
     
     
     input file sample:
-    *|[section1]|:          test
-    *|[section2]|:     
+    *|[Section 1]|:
+        content
+        *|2[heading 1]|
+         content
+        *|2[heading 1]|
+         content
+         content
+    *|[Section 2]|:  
     *|[end]|:
     
 '''
@@ -21,42 +27,57 @@ numberedSection = {'权利要求书':'。',
                     '说明书':'\n'}
 
 s = open(inputFile,encoding='utf-8').readlines()
-m = re.findall(r'\[(.+?)\]\|:(.+?)\*\|',"".join(s),flags=re.DOTALL)
-d = {key.strip():val.strip() for key, val in m}
+m = re.findall(r'==(.+?)====\n(.+?)\n==',"".join(s),flags=re.DOTALL)
+d = {key.strip().replace('=',''):val.strip() for key, val in m if key.strip()!= '' or val.strip()!=""}
+
 ns = {}
 for k in numberedSection.keys():
     v = numberedSection[k]
     if k in d:
-        ns[k] =[(i,line.strip()+ v) for i,line in enumerate(d[k].split(v)) if line.strip() != '' ]
-
+        #ns[k] =[(i,line.strip()+ v) for i,line in enumerate(d[k].split(v)) if line.strip() != '' ]
+        i = 0
+        ns[k] = []
+        for line in d[k].split(v):
+            if line.strip() == '':
+                continue
+            m = re.match(r'----(.+?)----',line)
+            try:
+                if len(m.groups(0)) > 0:
+                    heading = m.groups(0)[0].strip()
+                    ns[k].append(('#',heading))
+                continue
+            except:
+                pass
+            i +=1
+            ns[k].append( (str(i), line.strip()) )
 d['numberedSection'] = ns
 print(d)
 #######Make work directory
 templateFileList = []
 try:
-	shutil.rmtree("./tmp") 
+    shutil.rmtree("./tmp") 
 except:
-	print('Delete template directory failed!')
-	exit()
+    print('Delete template directory failed!')
+    exit()
 try:
-	shutil.copytree("./Template","./tmp")
+    shutil.copytree("./Template","./tmp")
 except:
-	print('Copy template directory failed')
-	exit()
+    print('Copy template directory failed')
+    exit()
 for dirpath, dirnames, filenames in os.walk('./tmp'):
     for filename in filenames:
         fullpath = os.path.join(dirpath,filename)
         if '.xml' in filename:
-        	templateFileList.append((dirpath,filename))
+            templateFileList.append((dirpath,filename))
 
 #######Second, fill the template
 for dirpath, TEMPLATE_FILE in templateFileList:
-	templateLoader = jinja2.FileSystemLoader( searchpath= dirpath , encoding='utf-8')
-	templateEnv = jinja2.Environment( loader=templateLoader )
-	template = templateEnv.get_template( TEMPLATE_FILE)
-	outputText = template.render( d )
+    templateLoader = jinja2.FileSystemLoader( searchpath= dirpath , encoding='utf-8')
+    templateEnv = jinja2.Environment( loader=templateLoader )
+    template = templateEnv.get_template( TEMPLATE_FILE)
+    outputText = template.render( d )
 #######Save output file
-	f=open(os.path.join(dirpath,TEMPLATE_FILE) ,'w',encoding='utf-8')
-	f.write(outputText)
-	f.close() 
-	print(TEMPLATE_FILE + '  done!')
+    f=open(os.path.join(dirpath,TEMPLATE_FILE) ,'w',encoding='utf-8')
+    f.write(outputText)
+    f.close() 
+    print(TEMPLATE_FILE + '  done!')
