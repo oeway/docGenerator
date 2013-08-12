89c82ab86ee8364c44895fa7414fe1472d3a53d8
oeway <oeway007@gmail.com>
2013-08-12 19:53:59 +0800

    packed as functions

--- docGenerator.py
+++ docGenerator.py
@@ -27,7 +27,6 @@ import jinja2
 import zipfile
 
 imageWidth = 160
-inputFile = './Input/inputDoc.txt'
 numberedSection = {'权利要求书':'。',
                     '说明书':'。',
                     '说明书附图':'\n',
@@ -39,120 +38,7 @@ imageSectionPath = {'说明书附图':'100003',
                 '摘要附图':'100005'}
 
 imageNameBase = '1308041747'
-s = open(inputFile,encoding='utf-8').readlines()
-m = re.findall(r'==(.+?)====\n(.*?)==',"".join(s),flags=re.DOTALL)
-d = {key.strip().replace('=',''):val.strip() for key, val in m if key.strip()!= '' or val.strip()!=""}
-#print (d)
-ns = {}
-for k in numberedSection.keys():
-    v = numberedSection[k]
-    if k in d:
-        #ns[k] =[(i,line.strip()+ v) for i,line in enumerate(d[k].split(v)) if line.strip() != '' ]
-        i = 0
-        ns[k] = {}
-        heading = "root"
-        ns[k][heading] = []
-        lineClosed = True
-        for line in d[k].split('\n'):
-            if line.strip() == '':
-                continue
-            m = re.match(r'----(.+?)----',line,flags=re.DOTALL)
-            try:
-                if len(m.groups(0)) > 0:
-                    heading = m.groups(0)[0].strip()
-                    ns[k][heading] = []
-                    #print(m.groups(0))
-                    # if m.groups(0)[1].strip() != "":
-                    #     i +=1
-                    #     ns[k].append( {'index':i,'type':'text', 'value':m.groups(0)[1].strip()+v} )
-                continue
-            except:
-                pass
-            if heading != '':
-                if heading in ns[k]:
-                    if lineClosed:
-                        i +=1
-                        ns[k][heading].append({'index':i,'type':'text','content':line.strip()})
-                    else:
-                        ns[k][heading].append({'index':-1,'type':'text','content':line.strip()})
-                    lineClosed = line.strip()[-1] == v
-import datetime
-da = datetime.date.today()
-d['日期'] = {"年":da.year,"月":da.month,"日":da.day}
 
-d['numberedSection'] = ns
-#######Make work directory
-try:
-    shutil.rmtree("./Output") 
-except:
-    print('Delete template directory failed!')
-try:
-    shutil.copytree("./Template","./Output")
-except:
-    print('Copy template directory failed')
-    exit()
-
-######Process image file
-from PIL import Image
-imageIndex = 0        
-for sec in imageSectionPath.keys():
-    try:
-        images = d[sec].split('\n')
-    except:
-        continue
-    imgLst = []
-    for i,img in enumerate(images):
-        try:
-            imageIndex +=10
-            fileName, fileExtension = os.path.splitext(img)
-            imgName = imageNameBase + str(imageIndex)+fileExtension
-            imgDict = {}
-            if sec in d['numberedSection']:
-                imgDict = d['numberedSection'][sec]['root'][i]
-            imgDict['type'] = 'image'
-            imgDict['img-format'] = fileExtension.strip('.')
-            im = Image.open(os.path.join('Input',img))
-            imgDict['width'] = imageWidth
-            imgDict['height'] = int(1.0*im.size[1]*imgDict['width']/im.size[0])
-            #print(im.size)
-            imgDict['content'] = imgName
-            if sec in d:
-                d[sec] = imgName
-            imgLst.append(imgDict)
-            shutil.copy (os.path.join('Input',img),  os.path.join('Output',imageSectionPath[sec],imgName))
-            if os.path.isfile (os.path.join('Output',imageSectionPath[sec],imgName)): print("Copy File Success")
-        except:
-            pass
-
-    if sec in d['numberedSection']:
-        d['numberedSection'][sec]['root'] = imgLst
-        #print(imgLst)
-templateFileList = []
-
-for dirpath, dirnames, filenames in os.walk('./Output'):
-    for filename in filenames:
-        fullpath = os.path.join(dirpath,filename)
-        if '.xml' in filename:
-            templateFileList.append((dirpath,filename))
-
-#######Second, fill the template
-for dirpath, TEMPLATE_FILE in templateFileList:
-    try:
-        templateLoader = jinja2.FileSystemLoader( searchpath= dirpath , encoding='utf-8')
-        templateEnv = jinja2.Environment( loader=templateLoader )
-        template = templateEnv.get_template( TEMPLATE_FILE)
-        outputText = template.render( d )
-    #######Save output file
-        f=open(os.path.join(dirpath,TEMPLATE_FILE) ,'w',encoding='utf-8')
-        f.write(outputText)
-        f.close() 
-        print(TEMPLATE_FILE + '  done!')
-    except:
-        os.remove(os.path.join(dirpath,TEMPLATE_FILE))
-        print("Error when process " + TEMPLATE_FILE)
-
-
-#######Make Archive
 
 def toZip( file, filename):
     zip_file = zipfile.ZipFile(filename, 'w')
@@ -171,7 +57,129 @@ def addFolderToZip( zip_file, folder):
         elif os.path.isdir(full_path):
             print ('Entering folder: ' + str(full_path))
             addFolderToZip(zip_file, full_path)
-filename = d['发明名称']+'.zip'
-directory = 'Output'
-toZip(directory, filename)
-print('Done!')
+            
+
+def generate(inputFile):
+    s = open(inputFile,encoding='utf-8').readlines()
+    m = re.findall(r'==(.+?)====\n(.*?)==',"".join(s),flags=re.DOTALL)
+    d = {key.strip().replace('=',''):val.strip() for key, val in m if key.strip()!= '' or val.strip()!=""}
+    #print (d)
+    ns = {}
+    for k in numberedSection.keys():
+        v = numberedSection[k]
+        if k in d:
+            #ns[k] =[(i,line.strip()+ v) for i,line in enumerate(d[k].split(v)) if line.strip() != '' ]
+            i = 0
+            ns[k] = {}
+            heading = "root"
+            ns[k][heading] = []
+            lineClosed = True
+            for line in d[k].split('\n'):
+                if line.strip() == '':
+                    continue
+                m = re.match(r'----(.+?)----',line,flags=re.DOTALL)
+                try:
+                    if len(m.groups(0)) > 0:
+                        heading = m.groups(0)[0].strip()
+                        ns[k][heading] = []
+                        #print(m.groups(0))
+                        # if m.groups(0)[1].strip() != "":
+                        #     i +=1
+                        #     ns[k].append( {'index':i,'type':'text', 'value':m.groups(0)[1].strip()+v} )
+                    continue
+                except:
+                    pass
+                if heading != '':
+                    if heading in ns[k]:
+                        if lineClosed:
+                            i +=1
+                            ns[k][heading].append({'index':i,'type':'text','content':line.strip()})
+                        else:
+                            ns[k][heading].append({'index':-1,'type':'text','content':line.strip()})
+                        lineClosed = line.strip()[-1] == v
+    import datetime
+    da = datetime.date.today()
+    d['日期'] = {"年":da.year,"月":da.month,"日":da.day}
+
+    d['numberedSection'] = ns
+    #######Make work directory
+    try:
+        shutil.rmtree("./Output") 
+    except:
+        print('Delete template directory failed!')
+    try:
+        shutil.copytree("./Template","./Output")
+    except:
+        print('Copy template directory failed')
+        exit()
+
+    ######Process image file
+    from PIL import Image
+    imageIndex = 0        
+    for sec in imageSectionPath.keys():
+        try:
+            images = d[sec].split('\n')
+        except:
+            continue
+        imgLst = []
+        for i,img in enumerate(images):
+            try:
+                imageIndex +=10
+                fileName, fileExtension = os.path.splitext(img)
+                imgName = imageNameBase + str(imageIndex)+fileExtension
+                imgDict = {}
+                if sec in d['numberedSection']:
+                    imgDict = d['numberedSection'][sec]['root'][i]
+                imgDict['type'] = 'image'
+                imgDict['img-format'] = fileExtension.strip('.')
+                im = Image.open(os.path.join('Input',img))
+                imgDict['width'] = imageWidth
+                imgDict['height'] = int(1.0*im.size[1]*imgDict['width']/im.size[0])
+                #print(im.size)
+                imgDict['content'] = imgName
+                if sec in d:
+                    d[sec] = imgName
+                imgLst.append(imgDict)
+                shutil.copy (os.path.join('Input',img),  os.path.join('Output',imageSectionPath[sec],imgName))
+                if os.path.isfile (os.path.join('Output',imageSectionPath[sec],imgName)): print("Copy File Success")
+            except:
+                pass
+
+        if sec in d['numberedSection']:
+            d['numberedSection'][sec]['root'] = imgLst
+            #print(imgLst)
+    templateFileList = []
+
+    for dirpath, dirnames, filenames in os.walk('./Output'):
+        for filename in filenames:
+            fullpath = os.path.join(dirpath,filename)
+            if '.xml' in filename:
+                templateFileList.append((dirpath,filename))
+
+    #######Second, fill the template
+    for dirpath, TEMPLATE_FILE in templateFileList:
+        try:
+            templateLoader = jinja2.FileSystemLoader( searchpath= dirpath , encoding='utf-8')
+            templateEnv = jinja2.Environment( loader=templateLoader )
+            template = templateEnv.get_template( TEMPLATE_FILE)
+            outputText = template.render( d )
+        #######Save output file
+            f=open(os.path.join(dirpath,TEMPLATE_FILE) ,'w',encoding='utf-8')
+            f.write(outputText)
+            f.close() 
+            print(TEMPLATE_FILE + '  done!')
+        except:
+            os.remove(os.path.join(dirpath,TEMPLATE_FILE))
+            print("Error when process " + TEMPLATE_FILE)
+
+
+    #######Make Archive
+    filename = d['发明名称']+'.zip'
+    directory = 'Output'
+    toZip(directory, filename)
+    print('Done!')
+
+    
+if __name__ == '__main__':
+    inputFileName = './Input/inputDoc.txt'
+    generate(inputFileName)
